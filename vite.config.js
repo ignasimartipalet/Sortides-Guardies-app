1a29fcb21ac9739fa36974e859978aeabeb8af53
Ignasi Martí Palet <ignasimartipalet@gmail.com>
2026-05-21 12:40:14 +0200

    Add build date definition to vite.config.js
    Added a build date definition to the Vite config.

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  define: {
+    __BUILD_DATE__: JSON.stringify(
+      new Date().toLocaleString('ca-ES', {
+        day:   '2-digit',
+        month: 'long',
+        year:  'numeric',
+        hour:  '2-digit',
+        minute:'2-digit',
+      })
+    ),
+  },
 });
